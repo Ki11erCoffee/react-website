adb2a9bd3013303651392d83fa7d53762116873a
JLUXLABEL-Paul <44684944+Ki11erCoffee@users.noreply.github.com>
2021-11-26 20:14:59 -0800

    eddited navbar

---
+++ react-website/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function navbar() {
+    return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <link to='/' classname='navbar-logo'>
+                      
+                </link>
+            </div>
+            
+        </nav>
+    </>
+    )
+}
+
+export default navbar
