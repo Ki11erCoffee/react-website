9eab57d503512689de98a847d36337ab3e9e3cb4
JLUXLABEL-Paul <44684944+Ki11erCoffee@users.noreply.github.com>
2021-12-01 11:17:56 -0800

    fixed home page

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/sign-up" element={<SignUp />} />
